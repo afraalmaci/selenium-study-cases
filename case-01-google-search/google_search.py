7dd430e4c5f9e16e290a2a2984b75623cc67ce46
afraalmaci <afraalmaci@gmail.com>
2026-04-13 16:06:14 +0200

    Add: Case 1 - Google search automation with cookie handling

---
+++ case-01-google-search/google_search.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep 
+
+service = Service("/Users/afragurcan/Desktop/Afra/drivers/chromedriver")
+driver = webdriver.Chrome(service=service)
+wait = WebDriverWait(driver, 10)
+
+page_url = "https://www.google.com/"
+driver.get(page_url)
+driver.maximize_window()
+
+try:
+    accept_btn = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//button[.//div[text()='Alles accepteren']]"))
+    )
+    accept_btn.click()
+    sleep(1)
+except:
+    print("Cookie popup did not appear.")
+
+search_box = wait.until(
+    EC.presence_of_element_located((By.NAME, "q"))
+)
+search_box.send_keys("Python")
+search_box.send_keys(Keys.ENTER)
+
+sleep(3)
+driver.quit()
